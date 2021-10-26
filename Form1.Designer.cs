c6a9e73ff575ae6aae6a8eb0d28ab5d08433e767
HeAkbari <46066452+HeAkbari@users.noreply.github.com>
2021-10-26 13:57:17 +0330

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace AutoUpdater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.picProccessBar = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picProccessBar)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(0, 129);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(43, 25);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run App";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(49, 129);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(43, 25);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Download and update file";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(464, 34);
+            this.panel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(43, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 173);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(464, 22);
+            this.panel2.TabIndex = 2;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(189, 81);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(118, 14);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "بروز رسانی نرم افزار...";
+            // 
+            // picProccessBar
+            // 
+            this.picProccessBar.BackColor = System.Drawing.Color.Gray;
+            this.picProccessBar.Image = ((System.Drawing.Image)(resources.GetObject("picProccessBar.Image")));
+            this.picProccessBar.Location = new System.Drawing.Point(-36, 67);
+            this.picProccessBar.Name = "picProccessBar";
+            this.picProccessBar.Size = new System.Drawing.Size(535, 72);
+            this.picProccessBar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picProccessBar.TabIndex = 10;
+            this.picProccessBar.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(252)))), ((int)(((byte)(252)))));
+            this.ClientSize = new System.Drawing.Size(464, 195);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.picProccessBar);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "نرم افزار صیاد";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picProccessBar)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox picProccessBar;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Threading;
+using System.Management;
+
+namespace AutoUpdater
+{
+    public partial class Form1 : Form
+    {
+        string downoadfileName = @"\\10.0.85.129\Shared-MposFile\SedClientTools";
+        string sourcefileName = Application.StartupPath;
+        int myLastversion = 0;
+        List<Version> versionList = new List<Version>();
+        Updateconfig updateconfig = new Updateconfig();
+
+        public Form1()
+        {
+            InitializeComponent();
+            using (StreamReader r = new StreamReader(sourcefileName + @"\LastVersion.json"))
+            {
+                string json = r.ReadToEnd();
+                myLastversion = JsonConvert.DeserializeObject<List<Version>>(json).First().VersionNumber;
+            }
+            using (StreamReader r = new StreamReader(sourcefileName + @"\UpdateConfig.json"))
+            {
+                string json = r.ReadToEnd();
+                updateconfig = JsonConvert.DeserializeObject<List<Updateconfig>>(json).First();
+                downoadfileName = updateconfig.UpdateUrl;
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker1.RunWorkerAsync();
+        }
+        private bool Is_App_Exist()
+        {
+            Process[] runingProcess = Process.GetProcesses();
+            for (int i = 0; i < runingProcess.Length; i++)
+            {
+                // compare equivalent process by their name
+                if (runingProcess[i].ProcessName == "SedClientTools")
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        private void Run_App()
+        {
+            if (Is_App_Exist())
+            {
+                MessageBox.Show("یک نسخه از نرم افزار در حال اجرا است");
+                return;
+            }
+            // MessageBox.Show("5");
+            //Application.StartupPath + @"\Update.exe"
+            Process.Start(sourcefileName + @"\SedClientTools.exe");
+            Application.Exit();
+        }
+        private void Online_Update_File()
+        {            
+            if (Is_App_Exist())
+            {
+                var dialogResult = MessageBox.Show("یک نسخه از نرم افزار در حال اجرا است،آیا مایلید آن را ببندید", "بروز رسانی نرم افزار", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                if (dialogResult == DialogResult.No)
+                    Application.Exit();
+                else
+                {
+                    var proccess = Process.GetProcessesByName("SedClientTools");
+                    proccess.First().Kill();
+                    Thread.Sleep(1000);
+                    Online_Update_File();
+                }
+            }
+            //Download zip file
+            using (StreamReader r = new StreamReader(@"\\" + downoadfileName + @"\UpdateVersion.json"))
+            {
+                string json = r.ReadToEnd();
+                versionList = JsonConvert.DeserializeObject<List<Version>>(json);
+                versionList = versionList.OrderBy(t => t.VersionNumber).ToList();
+            }
+            // MessageBox.Show("1");
+            for (int i = 0; i < versionList.Count; i++)
+            {
+                var version = versionList.Where(t => t.VersionNumber == myLastversion + 1).FirstOrDefault();
+                if (version != null)
+                {
+                    var fileIsExist = File.Exists(sourcefileName + @"\Update\SedClientTools" + version.VersionNumber + ".zip");
+                    if (!fileIsExist)
+                        using (var client = new WebClient())
+                        {
+                            client.DownloadFile(@"\\" + version.Url, sourcefileName + @"\Update\SedClientTools" + version.VersionNumber + ".zip");
+                        }
+
+                    Extract_File(version);
+                    myLastversion += 1;
+                }
+            }
+            // MessageBox.Show("2");
+            Run_App();
+        }
+        private void Offline_Update_File()
+        {
+            if (Is_App_Exist())
+            {
+                var proccess = Process.GetProcessesByName("SedClientTools");
+                proccess.First().Kill();
+                Thread.Sleep(1000);
+                Offline_Update_File();
+            }
+            
+            var version = new Version { VersionNumber = myLastversion + 1 };
+            var fileIsExist = File.Exists(sourcefileName + @"\Update\SedClientTools" + version.VersionNumber + ".zip");
+            if (fileIsExist)
+                Extract_File(version);
+            Run_App();
+        }
+        private void Extract_File(Version version)
+        {
+            using (ZipArchive archive = ZipFile.OpenRead(sourcefileName + @"\Update\SedClientTools" + version.VersionNumber + ".zip"))
+            {
+                var files = archive.Entries;
+                foreach (var file in files)
+                {
+                    File.Delete(sourcefileName + @"\" + file.Name);
+                }
+
+                archive.ExtractToDirectory(sourcefileName);
+            }
+            var LastVersion = new List<Version>();
+            LastVersion.Add(new Version
+            {
+                VersionNumber = version.VersionNumber,
+            });
+
+            string output = Newtonsoft.Json.JsonConvert.SerializeObject(LastVersion, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(sourcefileName + @"\LastVersion.json", output);
+
+            File.Delete(sourcefileName + @"\Update\SedClientTools" + version.VersionNumber + ".zip");
+        }
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            var proccess1 = Process.GetProcessesByName("AutoUpdater");
+            var result = GetCommandLine(proccess1.First());
+            var state = result[result.Length - 1].ToString();
+            if (state == "1")
+                Offline_Update_File();
+            else
+                Online_Update_File();
+        }
+        private string GetCommandLine(Process process)
+        {
+            using (ManagementObjectSearcher searcher = new ManagementObjectSearcher("SELECT CommandLine FROM Win32_Process WHERE ProcessId=" + process.Id))
+            using (ManagementObjectCollection objects = searcher.Get())
+            {
+                return objects.Cast<ManagementBaseObject>().SingleOrDefault()?["CommandLine"]?.ToString();
+            }
+
+        }
+    }
+
+    public class Version
+    {
+        public int VersionNumber;
+        public string Url;
+    }
+    public class Updateconfig
+    {
+        public string UpdateUrl;
+        public int ForceUpdate;
+    }
+
+}
